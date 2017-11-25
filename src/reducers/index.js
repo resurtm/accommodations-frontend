911503183a3808b54e3f6003c4593a63e9ae2b7f
resurtm <resurtm@gmail.com>
2017-11-25 22:19:43 +0600

    reducers minor rework

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import {combineReducers} from 'redux';
-import roomsEditor from 'reducers/rooms-editor';
+import roomsEditor from './rooms-editor';
 
 const reTicketsApp = combineReducers({
   roomsEditor,
---
+++ src/reducers/rooms-editor.js
@@ -0,0 +1,78 @@
+import Immutable from 'seamless-immutable';
+import {DESELECT_DAYS, SELECT_DAY, SELECT_DAY_RANGE, SET_ACTIVE_ROOM, SET_ACTIVE_YEAR} from 'actions/rooms-editor';
+
+const defaultState = {
+  activeRoom: '',
+  activeYear: new Date().getFullYear(),
+  selectedDays: [],
+};
+
+const selectDaysRange = ([startMonth, startDay], [endMonth, endDay]) => {
+  const selectedDays = [];
+  if (startMonth === endMonth) {
+    for (let i = startDay; i <= endDay; i++) {
+      selectedDays.push([startMonth, i]);
+    }
+  } else {
+    for (let i = startDay; i <= 31; i++) {
+      selectedDays.push([startMonth, i]);
+    }
+    if (Math.abs(startMonth - endMonth) > 1) {
+      for (let i = startMonth + 1; i <= endMonth - 1; i++) {
+        for (let j = 1; j <= 31; j++) {
+          selectedDays.push([i, j]);
+        }
+      }
+    }
+    for (let i = 1; i <= endDay; i++) {
+      selectedDays.push([endMonth, i]);
+    }
+  }
+  return selectedDays;
+};
+
+const selectedDays = (state = [], action) => {
+  switch (action.type) {
+    case SELECT_DAY:
+      return [[action.month, action.day]];
+
+    case SELECT_DAY_RANGE:
+      return state.length === 0
+        ? [[action.month, action.day]]
+        : selectDaysRange(state[state.length - 1], [action.month, action.day]);
+
+    case DESELECT_DAYS:
+      return [];
+
+    default:
+      return state;
+  }
+};
+
+const roomsEditor = (state = defaultState, action) => {
+  switch (action.type) {
+    case SET_ACTIVE_ROOM:
+      return Immutable.merge(state, {
+        activeRoom: action.room,
+        selectedDays: [],
+      });
+
+    case SET_ACTIVE_YEAR:
+      return Immutable.merge(state, {
+        activeYear: action.year,
+        selectedDays: [],
+      });
+
+    case SELECT_DAY:
+    case SELECT_DAY_RANGE:
+    case DESELECT_DAYS:
+      return Immutable.merge(state, {
+        selectedDays: selectedDays(state.selectedDays, action),
+      });
+
+    default:
+      return state;
+  }
+};
+
+export default roomsEditor
